a8c24bb4fa0ad828ec6d134ae41c4e8279503fe9
Jesse Bees <jesse@toomanybees.com>
2018-08-12 21:34:22 -0400

    don't pollute window with local vars in js

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,131 +1,135 @@
-class Hangman {
-  constructor(string, { win = this.win, lose = this.lose, onload } = {}) {
-    this.stringEncoder = new TextEncoder('utf-8');
-    this.stringDecoder = new TextDecoder('utf-8');
-    WebAssembly.instantiateStreaming(fetch("hangman.wasm"), {
-      env: {
-        win: win.bind(this),
-        lose: lose.bind(this),
-      }
-    })
-    .then(wasm => {
-      this.module = wasm.instance.exports;
-      const secretPhrase = string.split(/\s+/).join("").toLowerCase();
-      const { ptr, len } = this.newString(secretPhrase);
-      this.handle = this.module.init(ptr, len);
-      this.module.dealloc(ptr, len);
-      console.info(`Initialized game with phrase ${secretPhrase}`);
-    })
-    .then(() => onload(this));
-  }
-
-  // The public methods we want to expose
-
-  guess(char) {
-    let { ptr, len } = this.newString(char.toLowerCase());
-    const res = this.module.guess(this.handle, ptr, len);
-    this.module.dealloc(ptr, len);
-    return res;
-  }
-
-  unmasked() {
-    let ptr = this.module.unmasked(this.handle);
-    return this.readCString(ptr);
-  }
-
-  guessesRemaining() {
-    return this.module.guesses_remaining(this.handle);
-  }
-
-  // Encode a string in utf-8 bytes, allocate space for it
-  // in Wasm memory, then store it in the space provided.
-  // Return the address and length.
-  newString(string) {
-    const stringBytes = this.stringEncoder.encode(string);
-    const len = stringBytes.byteLength;
-    const ptr = this.module.alloc(len);
-    const memory = new Uint8Array(this.module.memory.buffer);
-    for (let i = 0; i < len; i++) {
-      memory[ptr + i] = stringBytes[i];
-    }
-    return { ptr, len };
-  }
-
-  // Read a null-terminated string from Wasm memory, given
-  // a pointer to the start of the string. Deallocates
-  // the string when complete.
-  readCString(ptr) {
-    const memory = new Uint8Array(this.module.memory.buffer, ptr);
-    function* getCStringBytes(ptr) {
-      let offset = 0;
-      while (memory[offset] !== 0) {
-        if (memory[offset] === undefined) {
-          throw new Error(`String ${ptr} continued into undefined memory at ${ptr+offset}`);
+(function(root) {
+  class Hangman {
+    constructor(string, { win = this.win, lose = this.lose, onload } = {}) {
+      this.stringEncoder = new TextEncoder('utf-8');
+      this.stringDecoder = new TextDecoder('utf-8');
+      WebAssembly.instantiateStreaming(fetch("hangman.wasm"), {
+        env: {
+          win: win.bind(this),
+          lose: lose.bind(this),
         }
-        yield memory[offset];
-        offset++;
+      })
+      .then(wasm => {
+        this.module = wasm.instance.exports;
+        const secretPhrase = string.split(/\s+/).join("").toLowerCase();
+        const { ptr, len } = this.newString(secretPhrase);
+        this.handle = this.module.init(ptr, len);
+        this.module.dealloc(ptr, len);
+        console.info(`Initialized game with phrase ${secretPhrase}`);
+      })
+      .then(() => onload(this));
+    }
+
+    // The public methods we want to expose
+
+    guess(char) {
+      let { ptr, len } = this.newString(char.toLowerCase());
+      const res = this.module.guess(this.handle, ptr, len);
+      this.module.dealloc(ptr, len);
+      return res;
+    }
+
+    unmasked() {
+      let ptr = this.module.unmasked(this.handle);
+      return this.readCString(ptr);
+    }
+
+    guessesRemaining() {
+      return this.module.guesses_remaining(this.handle);
+    }
+
+    // Encode a string in utf-8 bytes, allocate space for it
+    // in Wasm memory, then store it in the space provided.
+    // Return the address and length.
+    newString(string) {
+      const stringBytes = this.stringEncoder.encode(string);
+      const len = stringBytes.byteLength;
+      const ptr = this.module.alloc(len);
+      const memory = new Uint8Array(this.module.memory.buffer);
+      for (let i = 0; i < len; i++) {
+        memory[ptr + i] = stringBytes[i];
       }
+      return { ptr, len };
     }
 
-    let stringBytes = new Uint8Array(getCStringBytes(ptr));
-    let string = this.stringDecoder.decode(stringBytes);
-    this.module.dealloc_string(ptr, stringBytes.byteLength + 1);
-    return string;
-  }
+    // Read a null-terminated string from Wasm memory, given
+    // a pointer to the start of the string. Deallocates
+    // the string when complete.
+    readCString(ptr) {
+      const memory = new Uint8Array(this.module.memory.buffer, ptr);
+      function* getCStringBytes(ptr) {
+        let offset = 0;
+        while (memory[offset] !== 0) {
+          if (memory[offset] === undefined) {
+            throw new Error(`String ${ptr} continued into undefined memory at ${ptr+offset}`);
+          }
+          yield memory[offset];
+          offset++;
+        }
+      }
 
-  win() {
-    console.log("a winner is you");
-  }
-
-  lose() {
-    console.log("you have lost and dishonored your ancestors");
-  }
-
-  byteView() {
-    return new ByteView(this);
-  }
-}
-
-class ByteView {
-  constructor(game) {
-    this.game = game;
-    this.module = game.module;
-    this.cache = {};
-  }
-
-  invalidated() {
-    return !this.cache.array || !this.cache.array.buffer.byteLength;
-  }
-
-  get array() {
-    if (!this.cache.array || !this.cache.array.buffer.byteLength) {
-      this.cache = {
-        array: new Uint8ClampedArray(this.module.memory.buffer),
-      };
+      let stringBytes = new Uint8Array(getCStringBytes(ptr));
+      let string = this.stringDecoder.decode(stringBytes);
+      this.module.dealloc_string(ptr, stringBytes.byteLength + 1);
+      return string;
     }
-    return this.cache.array;
-  }
 
-  get hangman() {
-    if (this.invalidated() || !this.cache.hangman) {
-      const hangman = new Uint32Array(this.array.buffer, this.game.handle, this.module.size_of()/4);
-      this.cache.hangman = hangman;
+    win() {
+      console.log("a winner is you");
     }
-    return this.cache.hangman;
+
+    lose() {
+      console.log("you have lost and dishonored your ancestors");
+    }
+
+    byteView() {
+      return new ByteView(this);
+    }
   }
 
-  get phrase() {
-    const hangman = this.hangman;
-    return new Uint32Array(this.array.buffer, hangman[0], hangman[1]);
+  class ByteView {
+    constructor(game) {
+      this.game = game;
+      this.module = game.module;
+      this.cache = {};
+    }
+
+    invalidated() {
+      return !this.cache.array || !this.cache.array.buffer.byteLength;
+    }
+
+    get array() {
+      if (!this.cache.array || !this.cache.array.buffer.byteLength) {
+        this.cache = {
+          array: new Uint8ClampedArray(this.module.memory.buffer),
+        };
+      }
+      return this.cache.array;
+    }
+
+    get hangman() {
+      if (this.invalidated() || !this.cache.hangman) {
+        const hangman = new Uint32Array(this.array.buffer, this.game.handle, this.module.size_of()/4);
+        this.cache.hangman = hangman;
+      }
+      return this.cache.hangman;
+    }
+
+    get phrase() {
+      const hangman = this.hangman;
+      return new Uint32Array(this.array.buffer, hangman[0], hangman[1]);
+    }
+
+    get mask() {
+      const hangman = this.hangman;
+      return new Uint8ClampedArray(this.array.buffer, hangman[3], hangman[4]);
+    }
+
+    get guessed() {
+      const hangman = this.hangman;
+      return new Uint32Array(this.array.buffer, hangman[6], hangman[7]);
+    }
   }
 
-  get mask() {
-    const hangman = this.hangman;
-    return new Uint8ClampedArray(this.array.buffer, hangman[3], hangman[4]);
-  }
-
-  get guessed() {
-    const hangman = this.hangman;
-    return new Uint32Array(this.array.buffer, hangman[6], hangman[7]);
-  }
-}
+  root.Hangman = Hangman;
+})(window);
--- hangman/index.js
+++ hangman/index.js
@@ -1,74 +1,78 @@
-const initModal = document.getElementById("init-modal");
-const initForm = document.getElementById("init-form");
-const inputField = document.getElementById("input-field");
-const input = document.getElementById("input");
-const graphic = document.getElementById("graphic");
-const unmasked = document.getElementById("unmasked");
-const wrongGuesses = new Set();
-const wrongGuessesDom = document.getElementById("wrong-guesses");
+(function(root) {
+  const initModal = document.getElementById("init-modal");
+  const initForm = document.getElementById("init-form");
+  const inputField = document.getElementById("input-field");
+  const input = document.getElementById("input");
+  const graphic = document.getElementById("graphic");
+  const unmasked = document.getElementById("unmasked");
+  const wrongGuessesDom = document.getElementById("wrong-guesses");
 
-let game = null;
-function win() {
-  console.log("a winner is you!");
-  inputField.disabled = true;
-  inputField.placeholder = "";
-  graphic.classList.add("rainbowned");
-}
-function lose() {
-  console.log("you have failed and dishonored your ancestors");
-  inputField.disabled = true;
-  inputField.placeholder = "";
-}
-
-function guess(game, char) {
-  if (game.guess(char)) {
-    displayPhrase(game);
-  } else {
-    addWrongLetter(char);
-    displayGraphic(game);
+  let game = null;
+  function win() {
+    console.log("a winner is you!");
+    inputField.disabled = true;
+    inputField.placeholder = "";
+    graphic.classList.add("rainbowned");
   }
-  Bytes.setHangman();
-  Bytes.setMask();
-  Bytes.setGuessed();
-}
-input.addEventListener("submit", function(e) {
-  e.preventDefault();
-  if (inputField.value) {
-    guess(game, inputField.value);
-    inputField.value = "";
+  function lose() {
+    console.log("you have failed and dishonored your ancestors");
+    inputField.disabled = true;
+    inputField.placeholder = "";
   }
-});
 
-function displayGraphic(game) {
-  const remaining = game.guessesRemaining();
-  graphic.setAttribute("guesses", remaining.toString());
-}
-
-function displayPhrase(game) {
-  const unmaskedPhrase = game.unmasked().replace(/ /g, "_").split("").join(" ");
-  unmasked.textContent = unmaskedPhrase;
-}
-
-function addWrongLetter(char) {
-  if (!wrongGuesses.has(char)) {
-    wrongGuesses.add(char);
-    wrongGuessesDom.textContent = Array.from(wrongGuesses).sort().join(" ");
+  function guess(game, char) {
+    if (game.guess(char)) {
+      displayPhrase(game);
+    } else {
+      addWrongLetter(char);
+      displayGraphic(game);
+    }
+    Bytes.setHangman();
+    Bytes.setMask();
+    Bytes.setGuessed();
   }
-}
+  input.addEventListener("submit", function(e) {
+    e.preventDefault();
+    if (inputField.value) {
+      guess(game, inputField.value);
+      inputField.value = "";
+    }
+  });
 
-initModal.classList.remove("disabled");
-initForm.addEventListener("submit", function(e) {
-  e.preventDefault();
-  const phrase = document.getElementById("init-input").value;
-  if (phrase) {
-    game = new Hangman(phrase, {
-      win,
-      lose,
-      onload: (game) => {
-        displayPhrase(game)
-        Bytes.init(game.byteView());
-        initModal.classList.add("disabled");
-      },
-    });
+  function displayGraphic(game) {
+    const remaining = game.guessesRemaining();
+    graphic.setAttribute("guesses", remaining.toString());
   }
-});
+
+  function displayPhrase(game) {
+    const unmaskedPhrase = game.unmasked().replace(/ /g, "_").split("").join(" ");
+    unmasked.textContent = unmaskedPhrase;
+  }
+
+  const wrongGuesses = new Set();
+  function addWrongLetter(char) {
+    if (!wrongGuesses.has(char)) {
+      wrongGuesses.add(char);
+      wrongGuessesDom.textContent = Array.from(wrongGuesses).sort().join(" ");
+    }
+  }
+
+  initModal.classList.remove("disabled");
+  initForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const phrase = document.getElementById("init-input").value;
+    if (phrase) {
+      game = new Hangman(phrase, {
+        win,
+        lose,
+        onload: (game) => {
+          displayPhrase(game)
+          Bytes.init(game.byteView());
+          initModal.classList.add("disabled");
+        },
+      });
+    }
+  });
+
+  root.guess = guess;
+})(window);
