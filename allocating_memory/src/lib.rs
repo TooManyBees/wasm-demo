463bc938c28d019a9ffa062653764fdba6159c30
Jesse Bees <jesse@toomanybees.com>
2018-08-21 00:15:16 -0400

    make allocating_memory more interesting

--- allocating_memory/src/lib.rs
+++ allocating_memory/src/lib.rs
@@ -21,11 +21,47 @@ pub extern fn oom(_: ::core::alloc::Layout) -> ! {
     unsafe { ::core::intrinsics::abort(); }
 }
 
-#[cfg(target_arch = "wasm32")]
-use alloc::prelude::Box;
+#[cfg(target_arch = "wasm32")] use alloc::prelude::Box;
+#[cfg(target_arch = "wasm32")] use core::mem;
+#[cfg(not(target_arch = "wasm32"))] use std::mem;
+
+#[repr(C)]
+pub struct State {
+    value: i32,
+    edit_count: u32,
+}
 
 #[no_mangle]
-pub extern fn store_number(number: i32) -> *const i32 {
-    let boxed = Box::new(number);
-    Box::into_raw(boxed)
+pub extern fn create(value: i32) -> *mut State {
+  let state = Box::new(State {
+    value,
+    edit_count: 0,
+  });
+  Box::into_raw(state)
+}
+
+#[no_mangle]
+pub extern fn set(handle: *mut State, value: i32) -> u32 {
+let mut state = unsafe { Box::from_raw(handle) };
+    state.value = value;
+    state.edit_count += 1;
+    let count = state.edit_count;
+    mem::forget(state);
+    count
+}
+
+#[no_mangle]
+pub extern fn increment(handle: *mut State, offset: i32) -> u32 {
+    let mut state = unsafe { Box::from_raw(handle) };
+    state.value += offset;
+    state.edit_count += 1;
+    let count = state.edit_count;
+    mem::forget(state);
+    count
+}
+
+#[no_mangle]
+pub extern fn consume(handle: *mut State) -> i32 {
+  let state = unsafe { Box::from_raw(handle) };
+  state.value
 }
